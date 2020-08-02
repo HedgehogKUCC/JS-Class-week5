900e54d5dc58866744178c60f2a0db8dfc96c291
HedgehogKU <ccc908925@gmail.com>
2020-08-02 20:14:12 +0800

    新增 中文匯入

--- all.js
+++ all.js
@@ -1,3 +1,8 @@
+// 匯入語系檔案
+import zh_TW from './zh_TW.js';
+
+// 加入至 VeeValidate 的設定檔案
+VeeValidate.localize('tw', zh_TW);
 
 // input 驗證
 Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
