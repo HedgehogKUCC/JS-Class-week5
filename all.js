874fdef469e31a0a42a23e27aaf8aec9203672a7
HedgehogKU <ccc908925@gmail.com>
2020-08-02 20:09:13 +0800

    添加 input 驗證

--- all.js
+++ all.js
@@ -1,1 +1,24 @@
-console.log("Hello World !!!");
+
+// input 驗證
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
+
+// 自定義設定檔案，錯誤的 className
+VeeValidate.configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid',
+  }
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    form: {
+      username: '',
+      email: '',
+      telephone: '',
+      address: '',
+      payment: '',
+    }
+  }
+})
